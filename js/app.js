5d2c2d96a26e73eaa47164e242fc238593cbf742
Eugene B <enbotnaru@gmail.com>
2018-05-24 16:02:34 -0700

    added a to-do list

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 /*
  * Create a list that holds all of your cards
  */
+const deck = [
+  'fa fa-paper-plane-o', 'fa fa-paper-plane-o',
+  'fa fa-diamond', 'fa fa-diamond',
+  'fa fa-bicycle', 'fa fa-bicycle',
+  'fa fa-anchor', 'fa fa-anchor',
+  'fa fa-bolt', 'fa fa-bolt',
+  'fa fa-cube', 'fa fa-cube',
+  'fa fa-leaf', 'fa fa-leaf',
+  'fa fa-bomb', 'fa fa-bomb'
+]
 
-
+// funciton to crate a ul
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -11,26 +21,25 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+function shuffle (array) {
+  var currentIndex = array.length, temporaryValue, randomIndex
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
 
-    return array;
+  return array
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
+*  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
